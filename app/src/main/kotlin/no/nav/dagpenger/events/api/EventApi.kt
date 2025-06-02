e3c31c7acd83cc6243ca42de5bc65fbe492accd0
André Roaldseth <andre.roaldseth@nav.no>
2025-06-02 18:01:07 +0200

    Revert "Lag en pre-stop hook i APIet"
    This reverts commit 7eaa21c2a2c902ddad514af2be578de107e0b4f0.

--- app/src/main/kotlin/no/nav/dagpenger/events/api/EventApi.kt
+++ app/src/main/kotlin/no/nav/dagpenger/events/api/EventApi.kt
@@ -5,7 +5,6 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.statuspages.StatusPages
-import io.ktor.server.request.path
 import io.ktor.server.request.receiveText
 import io.ktor.server.request.uri
 import io.ktor.server.response.respond
@@ -14,43 +13,23 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import mu.KotlinLogging
-import no.nav.dagpenger.events.duckdb.PeriodicTrigger
 import no.nav.dagpenger.events.ingestion.EventIngestor
 import org.slf4j.event.Level
 
-private val callLogger = KotlinLogging.logger("CallLogging")
+private val callLogg = KotlinLogging.logger("CallLogging")
 
-private val ignoredPaths =
-    setOf(
-        "/internal/isalive",
-        "internal/isready",
-    )
-
-fun Application.eventApi(
-    ingestor: EventIngestor,
-    trigger: PeriodicTrigger? = null,
-) {
+fun Application.eventApi(ingestor: EventIngestor) {
     installStatusPages()
     install(CallLogging) {
         level = Level.INFO
-        logger = callLogger
+        logger = callLogg
         disableDefaultColors()
-
-        filter { call ->
-            ignoredPaths.none { ignoredPath ->
-                call.request.path().startsWith(ignoredPath)
-            }
-        }
     }
 
     routing {
         route("/internal") {
             get("/isalive") { call.respond(HttpStatusCode.OK, "OK") }
             get("/isready") { call.respond(HttpStatusCode.OK, "OK") }
-            get("/stop") {
-                trigger?.stop()
-                call.respond(HttpStatusCode.OK, "Stopping server")
-            }
         }
 
         post("/event") {
